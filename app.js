e2688e9b6f3cf1c78550c6c65e79f52b4817e399
Said <42440852+said154@users.noreply.github.com>
2018-11-02 12:41:40 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,95 +1,70 @@
 let fs = require('fs');
 const express = require('express')
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
 const port = 3000
 
-app.use (bodyParser.urlencoded({extended: true}));
+
+//app.use (bodyParser.urlencoded({extended: true}));
+let urlencodedParser = bodyParser.urlencoded({ extended: false})
 app.use(express.static('public'));
 
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
-
 app.get('/', (req, res) => {
+	const users = require('./users.json')
+	res.render('index', {title: 'Homepage', users: users})
 
-	fs.readFile('users.json', (err, data) => {
-	  if (err){
-	  	throw err;
+});
+
+
+app.get('/users-ajax', (req, res) => {
+	res.send({users})
+
+})
+
+app.get('/users', function (req,res){
+	res.render('users',{ users: users})
+
+})
+
+
+
+app.get('/search',(req, res) => {
+	res.render('search');
+});
+
+
+
+app.post('/search', urlencodedParser, function(req, res) {
+	const data = req.body.Name
+	const users = require('./users.json')
+	const response = [];
+
+	users.forEach(user => {
+		if (user.firstname.indexOf(data) >= 0 ) {
+			response.push(user)
 		}
-	
-	let users = JSON.parse(data);
-    res.render('index', {title: 'Homepage', users: users})
-
 	})
+
+	res.send(response);
 });
 
-app.get('/form',(req, res) => {
-res.render('form')
+app.get('/add-user', (req, res) => {
+	res.render('add-user');
 
+});
+
+
+app.post('/add', urlencodedParser, function (req, res) {
+	const users = require('./users.json')
+	users.push(req.body);
+	fs.writeFile('users.json', JSON.stringify(users), function (err) {
+		if (err) throw err;
 	});
-
-app.post('/form', (req, res) => {
-fs.readFile('users.json', (err, data) => {
-	  if (err){
-	  	throw err;
-		}
-	
-	let users = JSON.parse(data);
-	let name = req.body;
- res.render('input', {users: users, name: name})
-
- })
-
-});
-
-app.get('/add',(req, res) => {
-	res.render('add')
-});
-
-app.post('/add', (req, res) => {
-
-	  let enter = req.body
-	  console.log(enter)
-
-
-fs.readFile('users.json', (err, data) => {
-	  if (err){
-	  	throw err;
-		}
-	
-	let inputUser = JSON.parse(data);
-	inputUser.push(enter);
-	// res.redirect('/')
-	// });
-   
-
-    fs.writeFile('users.json', JSON.stringify(inputUser),(err,data) => {
-     if (err){
-       throw err;
-     }
-   })
-   res.redirect('/')
-
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	res.redirect('/')
+})
 
 
  app.listen(port, () => console.log(`Example app listening on port 3000!`))
